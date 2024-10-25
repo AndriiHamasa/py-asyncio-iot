0b8568d8f0b502c34c875f6ff6091f31a2e8e35b
Andrii <andrii.khamaza0@gmail.com>
2024-10-25 18:27:49 +0300

    fixed mistakes

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,4 +1,3 @@
-import asyncio
 import random
 import string
 from typing import Protocol
@@ -10,11 +9,9 @@ def generate_id(length: int = 8) -> str:
     return "".join(random.choices(string.ascii_uppercase, k=length))
 
 
-# Protocol is very similar to ABC, but uses duck typing
-# so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
 class Device(Protocol):
     async def connect(self) -> None:
-        ...  # Ellipsis - similar to "pass", but sometimes has different meaning
+        ...
 
     async def disconnect(self) -> None:
         ...
@@ -42,23 +39,9 @@ class IOTService:
 
     async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
-        tasks = {}
         for msg in program:
-            if msg.device_id not in tasks:
-                tasks[msg.device_id] = []
-            tasks[msg.device_id].append(msg)
-
-        await asyncio.gather(
-            *(self.run_device_program(device_id, device_msgs) for
-              device_id, device_msgs in tasks.items()))
-        print("=====END OF PROGRAM======")
-
-    async def run_device_program(
-            self, device_id: str,
-            messages: list[Message]
-    ) -> None:
-        for msg in messages:
             await self.send_msg(msg)
+        print("=====END OF PROGRAM======")
 
     async def send_msg(self, msg: Message) -> None:
         await self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
